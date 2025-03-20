8c63fcb2cdfecfd154b52b9d111f5845488e7127
lonycell <lonycell@gmail.com>
2025-03-20 16:47:07 +0900

    BRANDING #1

--- src/const/branding.ts
+++ src/const/branding.ts
@@ -2,12 +2,12 @@
 // if you want to use it in the commercial usage
 // please contact us for more information: hello@lobehub.com
 
-export const LOBE_CHAT_CLOUD = 'LobeChat Cloud';
+export const LOBE_CHAT_CLOUD = 'UtopChat Cloud';
 
-export const BRANDING_NAME = 'LobeChat';
+export const BRANDING_NAME = '유탑챗';
 export const BRANDING_LOGO_URL = '';
 
-export const ORG_NAME = 'LobeHub';
+export const ORG_NAME = 'UTOPSOFT';
 
 export const BRANDING_URL = {
   help: undefined,
